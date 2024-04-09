46aea73a4d2db8ceba816d98c97449ccb9f2a70a
George Oastler <goastler4@gmail.com>
2024-04-09 11:32:16 +0100

    command and args

--- packages/prosoponator-bot/src/review.ts
+++ packages/prosoponator-bot/src/review.ts
@@ -8,6 +8,10 @@ import * as github from '@actions/github'
 async function run() {
     console.log(':robot: brrr :robot:')
 
+    if (github.context.eventName !== 'issue_comment') {
+        console.log('This event is not a comment.');
+    }
+
     const comment = github.context.payload.comment
     if (!comment) {
         console.log('No comment found in payload')
@@ -16,10 +20,12 @@ async function run() {
 
     console.log('comment', comment)
 
-    // const body = comment.body
-    // const words = body.split(' ').map(word => word.trim()).filter(word => word.length > 0)
-    // const command = words[0]
-    // const args = words.slice(1)
+    const body = comment['body'] as string
+    const words = body.split(' ').map(word => word.trim()).filter(word => word.length > 0)
+    const command = words[0]
+    const args = words.slice(1)
+    console.log('command', command)
+    console.log('args', args)
 }
 
 run().catch(error => {
