2f5555323da616af363331fd4fb585fb5f5eb0d3
George Oastler <goastler4@gmail.com>
2025-01-07 11:21:55 +0000

    Update index.js

--- .github/actions/print_contexts/index.js
+++ .github/actions/print_contexts/index.js
@@ -8,6 +8,9 @@ function dotPrefix(prefix, key) {
 }
 
 function* iterEntries(obj, prefix = '') {
+	if(!obj) {
+		return;
+	}
 	for(const [key, value] of Object.entries(obj)) {
 		const prefixedKey = dotPrefix(prefix, key);
 		if(typeof value === 'object') {
